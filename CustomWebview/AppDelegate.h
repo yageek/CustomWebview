fcf6846dc310f0de3ecd3ec6ba9f9a267cb50c34
Yannick heinrich <yheinrich@openwt.com>
2015-03-17 22:29:26 +0100

    Initial Commit

---
+++ CustomWebview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CustomWebview
+//
+//  Created by Yannick Heinrich on 17/03/15.
+//  Copyright (c) 2015 yageek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CustomWebview/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomWebview
+//
+//  Created by Yannick Heinrich on 17/03/15.
+//  Copyright (c) 2015 yageek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
